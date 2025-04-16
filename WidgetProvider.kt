752daa6d718302954a9b1714e11e3d663c98d093
GreenPigGod <shenmingtianzhong450@gmail.com>
2025-04-16 18:01:05 +0900

    Create WidgetProvider.kt

---
+++ WidgetProvider.kt
@@ -0,0 +1,8 @@
+class WidgetProvider : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        for (appWidgetId in appWidgetIds) {
+            val views = RemoteViews(context.packageName, R.layout.widget_layout)
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
